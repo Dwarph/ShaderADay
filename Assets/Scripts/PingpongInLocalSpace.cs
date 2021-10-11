56609a4f401076f6cace55bd99c538460e8ad14b
pip.turner <pip.turner@ultraleap.com>
2021-10-11 09:34:38 +0100

    InputVector Shaders

---
+++ Assets/Scripts/PingpongInLocalSpace.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingpongInLocalSpace : MonoBehaviour
+{
+    public Vector3 LocalSpaceOffset = Vector3.zero;
+    private Vector3 startPos, endPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(startPos, startPos - LocalSpaceOffset,  Mathf.Abs(Mathf.Sin(Time.time)));
+    }
+}
