727d253bbe717dadc1b40702a3a36bc035a67d50
pip.turner <pip.turner@ultraleap.com>
2021-10-12 08:58:29 +0100

    Add different spaces shaders

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
+    public enum Direction { UP, LEFT, RIGHT, DOWN }
     public float rotationSpeed = 20;
+    public Direction direction = Direction.UP;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate ( Vector3.up * ( rotationSpeed * Time.deltaTime ) );
+        switch (direction)
+        {
+            case Direction.UP:
+                transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
+                break;
+            case Direction.LEFT:
+                transform.Rotate(Vector3.left * (rotationSpeed * Time.deltaTime));
+                break;
+            case Direction.RIGHT:
+                transform.Rotate(Vector3.right * (rotationSpeed * Time.deltaTime));
+                break;
+            case Direction.DOWN:
+                transform.Rotate(Vector3.down * (rotationSpeed * Time.deltaTime));
+                break;
+        }
     }
 }
